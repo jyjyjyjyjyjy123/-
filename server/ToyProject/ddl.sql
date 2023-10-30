2048c87854c77fa196f69dff3661e68ee2fd121d
jyjyjyjyjyjy123 <jly72552428@gmail.com>
2023-10-30 17:50:50 +0900

    231030

---
+++ server/ToyProject/ddl.sql
@@ -0,0 +1,19 @@
+--system에서 실행
+create user toy identified by java1234;
+
+grant connect, RESOURCE, dba to toy;
+
+-- ToyProject
+-- 회원
+create table tblUser (
+    id varchar2(50) not null,
+    pw varchar2(50) not null,
+    name varchar2(50) not null,
+    email varchar2(100) not null,
+    lv char(1) not null,
+    pic varchar2(100) default 'pic.png' not null,
+    intro varchar2(500) not null,
+    ing char(1) default 'y' not null,
+    constraint tbluser_pk primary key(id)
+);
+
